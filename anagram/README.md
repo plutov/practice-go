c180c643313c1c183aae401a251dd78a9efd081c
Aliaksandr Pliutau <pliutau.aliaksandr@lazada.com>
2017-02-28 17:49:38 +0700

    exercise 3: anagram

---
+++ anagram/README.md
@@ -0,0 +1,14 @@
+### Anagrams
+
+When two or more words are composed of the same characters, but in a different order, they are called [anagrams](https://en.wikipedia.org/wiki/Anagram). Write a function `FindAnagrams` that will find all possible anagrams for the given string in a given dictionary.
+
+Sample anagram:
+```
+"Madam Curie" = "Radium came"
+```
+
+### Run tests with benchmarks
+
+```
+go test -bench .
+```
---
+++ anagram/anagram_test.go
@@ -0,0 +1,46 @@
+package anagram
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+var dictionary []string
+
+var tests = []struct {
+	word     string
+	expected []string
+}{
+	{"", []string{}},
+}
+
+func init() {
+	content, err := ioutil.ReadFile("dictionary.txt")
+	if err == nil {
+		dictionary = strings.Split(string(content), "\n")
+	}
+}
+
+func TestFindAnagrams(t *testing.T) {
+	for _, test := range tests {
+		actual := FindAnagrams(dictionary, test.word)
+		if len(actual) != len(test.expected) {
+			t.Fatalf("FindAnagrams(%s) expected length %d, got %d", test.word, len(test.expected), len(actual))
+		}
+
+		for k, v := range test.expected {
+			if actual[k] != v {
+				t.Fatalf("FindAnagrams(%s) expected %v, got %v", test.word, test.expected, actual)
+			}
+		}
+	}
+}
+
+func BenchmarkTestFloydTriangle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range tests {
+			FindAnagrams(dictionary, test.word)
+		}
+	}
+}
