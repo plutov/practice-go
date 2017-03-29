1e7d2f2f86619285476c109ce49e166ff802a92d
Bruce Ediger <bediger@stratigery.com>
2017-03-29 08:26:23 -0600

    Remove commented-out-code, better comments, remove checks that always pass, rename some short, cryptic variables.

--- buildword/buildword.go
+++ buildword/buildword.go
@@ -6,77 +6,77 @@ import (
 	"strconv"
 )
 
-// BuildWord("buildword", []string{"buil", "dwor", "bu", "uild", "wo", "rd"}) = 4
-
+// Possible fragments of desired word,
+// in usable form.
 type Fragment struct {
-	frag   string
-	index  int
-	length int
+	index  int // Where fragment fits in desired word
+	length int // string length of fragment
 }
 
 func BuildWord(word string, fragments []string) int {
+
 	var candidates []Fragment
 	lw := len(word)
 	contents := make(map[string]bool)
+
 	for _, frag := range fragments {
 		lf := len(frag)
+		// Find all indexes into argument word where
+		// this fragment could possibly fit. A given
+		// fragment might match more than one index
+		// into word.
 		for i := 0; i+lf <= lw; i++ {
-			f := word[i : i+lf]
-			if f == frag && !contents[frag+strconv.Itoa(i)] {
-				var n Fragment
-				n.frag = frag
-				n.index = i
-				n.length = lf
-				candidates = append(candidates, n)
-				contents[frag+strconv.Itoa(i)] = true
+			matchingWordPart := word[i : i+lf]
+			if matchingWordPart == frag {
+				indexSuffix := strconv.Itoa(i)
+				if !contents[frag+indexSuffix] {
+					var n Fragment
+					n.index = i
+					n.length = lf
+					candidates = append(candidates, n)
+					contents[frag+indexSuffix] = true
+				}
 			}
 		}
 	}
 
-/*
-	for _, cand := range candidates {
-		fmt.Printf("%q at %d, length %d\n", cand.frag, cand.index, cand.length)
-	}
-*/
-
 	var min int = math.MaxInt32
 	if len(candidates) > 0 {
-		min = ChooseFragment("", 0, len(word), candidates, 0)
+		min = ChooseFragment(0, len(word), candidates, 0)
 	}
 
-	if min == math.MaxInt32 { return 0 }
+	if min == math.MaxInt32 {
+		return 0
+	}
 	return min
 }
 
-func ChooseFragment(sofar string, position int, length int, candidates []Fragment, fragCount int) int {
-	// fmt.Printf("Enter ChooseFragment(%q, %d, %d, %d)\n", sofar, position, length, fragCount)
-	// from candidates[] find all entries that start at position, and
-	// don't end up longer than length. If there's any length left over,
-	// call recursively.
+// from candidates[] find all entries that start at position, and
+// don't end up longer than length. If there's any length left over,
+// call recursively.
+func ChooseFragment(position int, length int, candidates []Fragment, fragCount int) int {
+
+	// Fragments that match substring of desired word at index position.
 	var curr []Fragment
 
 	for _, frag := range candidates {
-		if frag.index == position && position+frag.length <= length {
+		if frag.index == position {
 			if position+frag.length == length {
-				fragCount++
-				// fmt.Printf("-> Returning %d on fragment %q, %q\n", fragCount, frag, sofar)
-				return fragCount
+				// This fragment matches substring of desired word,
+				// and it completes the desired word.
+				return fragCount + 1
 			}
 			curr = append(curr, frag)
 		}
 	}
-	// fmt.Printf("\tChooseFragment(%q, %d, %d) from among %q\n", sofar, position, length, curr)
 
 	var min int = math.MaxInt32
 
-	if len(curr) > 0 {
-		for _, frag := range curr {
-			if position+frag.length <= length {
-				m := ChooseFragment(sofar+frag.frag, position+frag.length, length, candidates, fragCount+1)
-
-				if m < min {
-					min = m
-				}
+	for _, frag := range curr {
+		if position+frag.length <= length {
+			m := ChooseFragment(position+frag.length, length, candidates, fragCount+1)
+			if m < min {
+				min = m
 			}
 		}
 	}
