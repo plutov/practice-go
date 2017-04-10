a47d278edc4d6505c7042639248bdab5be13ae38
Ivan Kurnosov <zerkms@zerkms.com>
2017-04-10 18:35:34 +1200

    Added support for 900, 90, 40 and 9

--- romannumerals/romannumerals.go
+++ romannumerals/romannumerals.go
@@ -9,11 +9,15 @@ type romanArabic struct {
 
 var numbers = []romanArabic{
 	{"M", 1000},
+	{"CM", 900},
 	{"D", 500},
 	{"CD", 400},
 	{"C", 100},
+	{"XC", 90},
 	{"L", 50},
+	{"XL", 40},
 	{"X", 10},
+	{"IX", 9},
 	{"V", 5},
 	{"IV", 4},
 	{"I", 1},
