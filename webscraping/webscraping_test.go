5b30554e2c3b75dd9a880acd03ab5d2ee02afe88
Ivan Kurnosov <zerkms@zerkms.com>
2017-04-20 09:17:14 +1200

    Fixed webscraping tests to respect current time

--- webscraping/webscraping_test.go
+++ webscraping/webscraping_test.go
@@ -1,6 +1,19 @@
 package webscraping
 
 import "testing"
+import "time"
+
+import "strings"
+
+var timezones = map[string]string{
+	"UTC":  "UTC",
+	"PDT":  "US/Pacific",
+	"MDT":  "US/Mountain",
+	"CDT":  "US/Central",
+	"EDT":  "US/Eastern",
+	"AKDT": "US/Alaska",
+	"HAST": "US/Hawaii",
+}
 
 var tests = []struct {
 	timezone string
@@ -16,12 +29,65 @@ var tests = []struct {
 	{"AM", ""},
 }
 
+func generateTimesForTimezone(timezone string, span int) ([]string, error) {
+	result := []string{}
+
+	tzName, ok := timezones[timezone]
+	if !ok {
+		return result, nil
+	}
+
+	loc, err := time.LoadLocation(tzName)
+	if err != nil {
+		return result, err
+	}
+
+	baseTime := time.Now().In(loc)
+
+	for offset := -span; offset <= span; offset++ {
+		offsetTime := baseTime.Add(time.Duration(offset) * time.Second)
+
+		var format string
+		if timezone == "UTC" {
+			format = "Jan. 2, 15:04:05"
+		} else {
+			format = "Jan. 2, 15:04:05 PM"
+		}
+
+		formattedOffsetTime := offsetTime.Format(format) + " " + timezone
+		result = append(result, formattedOffsetTime)
+	}
+
+	return result, nil
+}
+
 func TestGetTime(t *testing.T) {
+	span := 10
+
 	for _, test := range tests {
 		actual := GetTime(test.timezone)
 
-		if test.expected != actual {
-			t.Fatalf("GetTime(\"%s\") expected %s, got %s", test.timezone, test.expected, actual)
+		expectedTimes, err := generateTimesForTimezone(test.timezone, span)
+		if err != nil {
+			t.Fatalf("%s", err)
+			continue
+		}
+
+		if len(expectedTimes) == 0 {
+			expectedTimes = append(expectedTimes, "")
+		}
+
+		found := false
+		for _, expected := range expectedTimes {
+			if expected == actual {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			expectedList := strings.Join(expectedTimes, ", ")
+			t.Fatalf("GetTime(\"%s\") expected one of %s, got %s", test.timezone, expectedList, actual)
 		}
 	}
 }
