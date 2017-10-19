e1543249bf699a77b5ca750f5f4cb4674e06bc13
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2017-10-18 20:27:57 -0400

    Implement secretmessage.Decode

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -2,5 +2,22 @@ package secretmessage
 
 // Decode func
 func Decode(encoded string) string {
-	return ""
+	// count
+	counts := make(map[rune]int)
+	for _, r := range encoded {
+		counts[r]++
+	}
+	// only get runes >= counts[_]
+	var runes []rune
+	for k, v := range counts {
+		if k != '_' && v >= counts['_'] {
+			runes = append(runes, k)
+		}
+	}
+	// sort runes by count
+	sort.Slice(runes, func(i, j int) bool {
+		return counts[runes[i]] > counts[runes[j]]
+	})
+	// return string
+	return string(runes)
 }
