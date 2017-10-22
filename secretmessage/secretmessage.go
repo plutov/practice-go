cef7dfe8e83665b12eceb316ede9bf8ca146b5f4
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2017-10-21 22:51:24 -0400

    Benchmark improvements
    1) allocate rune map with bucket size 27 
    2) allocate accumulator rune slice with capacity 26
    
    ```
    GOOS: darwin
    GOARCH: amd64
    
    # Before
    BenchmarkDecode-4   	   20000	     97820 ns/op	    1919 B/op	      13 allocs/op
    
    # After
    BenchmarkDecode-4   	   20000	     93227 ns/op	    1155 B/op	       7 allocs/op
    ```

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -4,19 +4,20 @@ import "sort"
 
 // Decode func
 func Decode(encoded string) string {
-	// count
-	counts := make(map[rune]int)
+	// count: size 27 for main case of alpha+_
+	counts := make(map[rune]int, 27)
 	for _, r := range encoded {
 		counts[r]++
 	}
-	// only get runes >= counts[_]
-	var runes []rune
+	// only get runes with count >= counts[_]
+	// cap 26 for base case of alpha chars
+	runes := make([]rune, 0, 26)
 	for k, v := range counts {
 		if k != '_' && v >= counts['_'] {
 			runes = append(runes, k)
 		}
 	}
-	// sort runes by count
+	// sort runes by count desc
 	sort.Slice(runes, func(i, j int) bool {
 		return counts[runes[i]] > counts[runes[j]]
 	})
