7b4d399202f6ad080b0a7db8ad354dbfd726d6e3
Abdallah Hodieb <hodieba@amazon.com>
2017-10-25 02:45:22 +0100

    Submit solution for secret message
    The solution:
    - creates a lookup table to count the runes in the string,
    - Sorts the runes from the lookup table by their count
    - Return back the string part before the first _
    
    ** Side Note: **
    I'm also looking for comments on coding style (variable names, using
    idiomatic go style), I'm a java developer and I've only been doing go
    for a week

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -1,6 +1,65 @@
 package secretmessage
 
+import "sort"
+
+//RuneCounter counts the frequency of runes using a lookup table
+type RuneCounter struct {
+	lookuptable map[rune]int
+}
+
+//NewRuneCounter create new rune counter
+func NewRuneCounter() RuneCounter {
+	return RuneCounter{
+		make(map[rune]int),
+	}
+}
+
+//Count counts the frequecy of a rune in a stirng
+func (counter *RuneCounter) Count(str string) {
+	for _, r := range str {
+		counter.lookuptable[r] = counter.lookuptable[r] + 1
+	}
+}
+
+//CountedRune represnts the rune counts
+type CountedRune struct {
+	Rune  rune
+	Count int
+}
+
+//Runes gets the CountedRunes
+func (counter RuneCounter) Runes() []CountedRune {
+	runes := make([]CountedRune, len(counter.lookuptable))
+	i := 0
+	for r, c := range counter.lookuptable {
+		runes[i] = CountedRune{r, c}
+		i++
+	}
+
+	return runes
+}
+
+//CountedRunes implements sort.Interface to sort counted runes by count
+type CountedRunes []CountedRune
+
+func (a CountedRunes) Len() int           { return len(a) }
+func (a CountedRunes) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a CountedRunes) Less(i, j int) bool { return a[i].Count < a[j].Count }
+
 // Decode func
 func Decode(encoded string) string {
-	return ""
+	counter := NewRuneCounter()
+	counter.Count(encoded)
+	runes := counter.Runes()
+
+	sort.Sort(sort.Reverse(CountedRunes(runes)))
+
+	var decoded []rune
+	for _, r := range runes {
+		if r.Rune == '_' {
+			break
+		}
+		decoded = append(decoded, r.Rune)
+	}
+	return string(decoded)
 }
