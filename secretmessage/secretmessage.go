a31c44f952052aec67f4e7b63d1e034ab8fadc3f
Ja Vouhey <gb.javouhey@gmail.com>
2017-10-25 20:11:56 -0700

    works 2

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -1,31 +1,68 @@
 package secretmessage
 
 import (
-    "fmt"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-// Encode func
+func InsertionSort(arg []string) {
+	i := 1
+	j := 0
+	var x string
+	var y, xval int
+	for i < len(arg) {
+		x = arg[i]
+		j = i - 1
+		y, _ = strconv.Atoi(strings.Split(arg[j], ":")[1])
+		xval, _ = strconv.Atoi(strings.Split(x, ":")[1])
+		for j >= 0 && y > xval {
+			arg[j+1] = arg[j]
+			j -= 1
+			if j >= 0 {
+				y, _ = strconv.Atoi(strings.Split(arg[j], ":")[1])
+			}
+		}
+		arg[j+1] = x
+		i += 1
+	}
+}
+
 func Encode(encoded string) string {
-        var keys []string
+	var keys []string
 	var counter map[string]int
-        var currentKey string
+	var currentKey string
 
 	counter = make(map[string]int)
 
 	for _, runeVal := range encoded {
-            currentKey = string(runeVal)
-            _, inMap := counter[currentKey]
-	    if !inMap {
-                counter[currentKey] = 1
-		keys = append(keys, currentKey)
-	    } else {
-                counter[currentKey] = counter[currentKey] + 1
-	    }
+		currentKey = string(runeVal)
+		_, inMap := counter[currentKey]
+		if !inMap {
+			counter[currentKey] = 1
+			keys = append(keys, currentKey)
+		} else {
+			counter[currentKey] = counter[currentKey] + 1
+		}
 	}
 
-	fmt.Println("%v", counter)
-	fmt.Println("%v", keys)
+	var temp []string
+	for k, v := range counter {
+		temp = append(temp, k+":"+fmt.Sprintf("%d", v))
+	}
+	InsertionSort(temp)
 
-	// @TODO do insertion sort on counter
-	return ""
+	var g string
+	var result []string
+
+	for z := len(temp) - 1; z >= 0; z-- {
+		g = strings.Split(temp[z], ":")[0]
+		if g == "_" {
+			break
+		} else {
+			result = append(result, g)
+		}
+	}
+
+	return strings.Join(result, "")
 }
