9f54c4f45d4a2dd33142afce3b6335c40fbb9731
Gladkikh Artem <artem.and.co@gmail.com>
2017-11-08 15:06:05 +0300

    Update missingnumbers.go
    BenchmarkMissing-4   	 1000000	      1373 ns/op

--- missingnumbers/missingnumbers.go
+++ missingnumbers/missingnumbers.go
@@ -2,5 +2,25 @@ package missingnumbers
 
 // Missing func
 func Missing(numbers []int) []int {
+	numbers = append(numbers, numbers[0], numbers[1])
+	for _, val := range numbers {
+		if val < 0 {
+			val = -val
+		}
+		if numbers[val-1] > 0 {
+			numbers[val-1] = -numbers[val-1]
+		}
+	}
+
+	n1 := 0
+	for i, val := range numbers {
+		if val > 0 {
+			if n1 == 0 {
+				n1 = i + 1
+			} else {
+				return []int{n1, i + 1}
+			}
+		}
+	}
 	return []int{}
 }
