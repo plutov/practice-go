1ce1c1365a3bc42a98006b11d6ff96096ed16cc3
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2019-08-31 11:09:37 -0300

    Node Degree Solution

--- node_degree/node_degree.go
+++ node_degree/node_degree.go
@@ -1,6 +1,22 @@
 package nodedegree
 
+import (
+	"fmt"
+)
+
 // Degree func
 func Degree(nodes int, graph [][2]int, node int) (int, error) {
-	return 0, nil
+	if node > nodes {
+		return 0, fmt.Errorf("node %d not found in the graph", node)
+	}
+
+	degree := 0
+
+	for _, n := range graph {
+		if n[0] == node || n[1] == node {
+			degree++
+		}
+	}
+
+	return degree, nil
 }
