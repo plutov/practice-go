ad9200f4a11cdbded481ab06dc34afbbca46e774
Matthew Gabeler-Lee <cheetah@fastcat.org>
2020-05-11 22:56:51 -0400

    18x speed boost to secretmessage
    before:
    ```
    $ go test -bench .
    goos: linux
    goarch: amd64
    BenchmarkDecode-12         21541             55585 ns/op
    ```
    after
    ```
    BenchmarkDecode-12        389108              2950 ns/op
    ```

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -5,21 +5,25 @@ import "sort"
 // Decode func
 func Decode(encoded string) string {
 	// count: size 27 for main case of alpha+_
-	counts := make(map[rune]int, 27)
+	counts := make([]int, 27)
 	for _, r := range encoded {
-		counts[r]++
+		if r == '_' {
+			counts[26]++
+		} else {
+			counts[r-'a']++
+		}
 	}
 	// only get runes with count >= counts[_]
 	// cap 26 for base case of alpha chars
 	runes := make([]rune, 0, 26)
 	for k, v := range counts {
-		if k != '_' && v >= counts['_'] {
-			runes = append(runes, k)
+		if k != 26 && v >= counts[26] {
+			runes = append(runes, rune('a'+k))
 		}
 	}
 	// sort runes by count desc
 	sort.Slice(runes, func(i, j int) bool {
-		return counts[runes[i]] > counts[runes[j]]
+		return counts[runes[i]-'a'] > counts[runes[j]-'a']
 	})
 	// return string
 	return string(runes)
