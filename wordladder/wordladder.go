faffe002b2d33a578d77a283dd391ea07b846cf5
dot4zero <dot4zero@gmail.com>
2022-05-26 18:11:24 +0500

    anargam+buildword+coins+wordladder

--- wordladder/wordladder.go
+++ wordladder/wordladder.go
@@ -1,97 +1,71 @@
 package wordladder
 
-import "math"
+import (
+	"github.com/eapache/queue"
+)
 
-// WordLadder is used to count the length of the shortest transformation sequence from first word to second word
-func WordLadder(from string, to string, dic []string) int {
-	if len(from) != len(to) {
-		return 0
-	}
-	u := indexInSlice(from, dic)
-	v := indexInSlice(to, dic)
-	if v == -1 {
-		return 0
-	}
-	if u == -1 {
-		u = len(dic)
-		dic = append(dic, from)
-	}
-	graph := generateGraph(dic)
-	return dijkstra(graph, u, v)
+type Pair struct {
+	s int
+	d int
 }
 
-func indexInSlice(s string, list []string) int {
-	for i := 0; i < len(list); i++ {
-		if s == list[i] {
-			return i
+func bfs(adj [][]int, s int, t int) int {
+	vis := make([]bool, len(adj))
+	vis[s] = true
+	q := queue.New()
+	q.Add(Pair{s, 0})
+	for q.Length() > 0 {
+		p := q.Peek().(Pair)
+		s = p.s
+		d := p.d
+		if s == t {
+			return d + 1
 		}
-	}
-	return -1
-}
-
-func generateGraph(dic []string) [][]int {
-	g := [][]int{}
-	for i := 0; i < len(dic); i++ {
-		r := []int{}
-		for j := 0; j < len(dic); j++ {
-			t := distance(dic[i], dic[j])
-			r = append(r, t)
-		}
-		g = append(g, r)
-	}
-	return g
-}
-
-func distance(s string, t string) int {
-	if len(s) != len(t) {
-		return 0
-	}
-	count := 0
-	for i := 0; i < len(s); i++ {
-		if s[i] != t[i] {
-			count++
-		}
-	}
-	if count == 1 {
-		return 1
-	}
-	return 0
-}
-
-func dijkstra(graph [][]int, u int, v int) int {
-	n := len(graph[0])
-	visited := make([]bool, n)
-	dist := make([]int, n)
-	for i := 0; i < n; i++ {
-		dist[i] = math.MaxInt64
-	}
-	dist[u] = 0
-	for i := 0; i < n; i++ {
-		t := minDistance(dist, visited)
-		if t == -1 {
-			return 0
-		}
-		if t == v {
-			return dist[t] + 1
-		}
-		visited[t] = true
-		for j := 0; j < n; j++ {
-			if !visited[j] && graph[t][j] == 1 && dist[t] != math.MaxInt64 && dist[t]+graph[t][j] < dist[j] {
-				dist[j] = dist[t] + graph[t][j]
+		q.Remove()
+		vis[s] = true
+		for _, v := range adj[s] {
+			if !vis[v] {
+				q.Add(Pair{v, d + 1})
 			}
 		}
 	}
 	return 0
 }
 
-func minDistance(dist []int, visited []bool) int {
-	min := math.MaxInt64
-	index := -1
-	for i := 0; i < len(dist); i++ {
-		if !visited[i] && dist[i] < min {
-			min = dist[i]
-			index = i
+func dist(str1 string, str2 string) bool {
+	res := false
+	for i := 0; i < len(str1); i++ {
+		if str1[i] != str2[i] {
+			if res {
+				return false
+			}
+			res = true
 		}
 	}
-	return index
+	return res
+}
+
+func add(str string, dic *[]string) int {
+	for i, s := range *dic {
+		if s == str {
+			return i
+		}
+	}
+	*dic = append(*dic, str)
+	return len(*dic) - 1
+}
+
+func WordLadder(from string, to string, dic []string) int {
+	s := add(from, &dic)
+	t := add(to, &dic)
+	adj := make([][]int, len(dic))
+	for i := 0; i < len(dic); i++ {
+		for j := i + 1; j < len(dic); j++ {
+			if dist(dic[i], dic[j]) {
+				adj[i] = append(adj[i], j)
+				adj[j] = append(adj[j], i)
+			}
+		}
+	}
+	return bfs(adj, s, t)
 }
