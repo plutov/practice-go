f3c7bad094ef5796157ec8b9de628244e6bdeb0d
dot4zero <dot4zero@gmail.com>
2022-06-03 10:22:10 +0500

    buildword

--- buildword/buildword.go
+++ buildword/buildword.go
@@ -1,84 +1,50 @@
 package buildword
 
 import (
-	"math"
-	"strconv"
+	"github.com/eapache/queue"
+	"strings"
 )
 
-// Possible fragments of desired word,
-// in usable form.
-type Fragment struct {
-	index  int // Where fragment fits in desired word
-	length int // string length of fragment
+type Pair struct {
+	s int
+	d int
+}
+
+func bfs(adj [][]int, s int, t int) int {
+	vis := make([]bool, len(adj))
+	vis[s] = true
+	q := queue.New()
+	q.Add(Pair{s, 0})
+	for q.Length() > 0 {
+		p := q.Peek().(Pair)
+		s = p.s
+		d := p.d
+		if s == t {
+			return d
+		}
+		q.Remove()
+		vis[s] = true
+		for _, v := range adj[s] {
+			if !vis[v] {
+				q.Add(Pair{v, d + 1})
+			}
+		}
+	}
+	return 0
 }
 
 func BuildWord(word string, fragments []string) int {
-
-	var candidates []Fragment
-	lw := len(word)
-	contents := make(map[string]bool)
-
-	for _, frag := range fragments {
-		lf := len(frag)
-		// Find all indexes into argument word where
-		// this fragment could possibly fit. A given
-		// fragment might match more than one index
-		// into word.
-		for i := 0; i+lf <= lw; i++ {
-			matchingWordPart := word[i : i+lf]
-			if matchingWordPart == frag {
-				indexSuffix := strconv.Itoa(i)
-				if !contents[frag+indexSuffix] {
-					var n Fragment
-					n.index = i
-					n.length = lf
-					candidates = append(candidates, n)
-					contents[frag+indexSuffix] = true
-				}
+	adj := make([][]int, len(word)+1)
+	for _, fragment := range fragments {
+		i, j := 0, 0
+		for j != -1 {
+			j = strings.Index(word[i:], fragment)
+			if j != -1 {
+				i += j
+				adj[i] = append(adj[i], i+len(fragment))
+				i += len(fragment)
 			}
 		}
 	}
-
-	var min int = math.MaxInt32
-	if len(candidates) > 0 {
-		min = ChooseFragment(0, len(word), candidates, 0)
-	}
-
-	if min == math.MaxInt32 {
-		return 0
-	}
-	return min
-}
-
-// from candidates[] find all entries that start at position, and
-// don't end up longer than length. If there's any length left over,
-// call recursively.
-func ChooseFragment(position int, length int, candidates []Fragment, fragCount int) int {
-
-	// Fragments that match substring of desired word at index position.
-	var curr []Fragment
-
-	for _, frag := range candidates {
-		if frag.index == position {
-			if position+frag.length == length {
-				// This fragment matches substring of desired word,
-				// and it completes the desired word.
-				return fragCount + 1
-			}
-			curr = append(curr, frag)
-		}
-	}
-
-	var min int = math.MaxInt32
-
-	for _, frag := range curr {
-		if position+frag.length <= length {
-			m := ChooseFragment(position+frag.length, length, candidates, fragCount+1)
-			if m < min {
-				min = m
-			}
-		}
-	}
-
-	return min
+	return bfs(adj, 0, len(word))
 }
