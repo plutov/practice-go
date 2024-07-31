20457fe8ec8679ca23f79ad3efef6ca0b8958988
Steve <stev3npy@gmail.com>
2024-07-31 22:02:18 +0300

    refactored tests

--- url_shortener/url_shortener_test.go
+++ url_shortener/url_shortener_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+func TestShortenAndExpand(t *testing.T) {
+	url := "https://example.com"
+	short := Shorten(url)
+	expanded := Expand(short)
+
+	if expanded != url {
+		t.Errorf("Expected %s, got %s", url, expanded)
+	}
+}
+
 func BenchmarkShorten(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Shorten("http://example.com")
