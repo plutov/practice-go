6b36d8bc8e257b13d48bbb72d5c1b6f74f32fd81
plutov <a.pliutau@gmail.com>
2025-12-10 21:23:29 +0100

    aoc 8

---
+++ aoc-2025/8/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Point3D struct {
+	X, Y, Z int
+	Index   int
+}
+
+type Circuit struct {
+	Points []int
+}
+
+func main() {
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(file), "\n")
+	lines = lines[:len(lines)-1]
+
+	points := make([]Point3D, len(lines))
+	for i, line := range lines {
+		coords := strings.Split(line, ",")
+		if len(coords) != 3 {
+			panic("Invalid input format")
+		}
+		p := Point3D{
+			Index: i,
+		}
+		p.X, _ = strconv.Atoi(coords[0])
+		p.Y, _ = strconv.Atoi(coords[1])
+		p.Z, _ = strconv.Atoi(coords[2])
+		points[i] = p
+	}
+
+	uniquePairs := make(map[[2]int]struct{})
+	pairs := [][2]Point3D{}
+	for i := range points {
+		for j := range points {
+			if i == j {
+				continue
+			}
+			if i > j {
+				continue
+			}
+			if _, exists := uniquePairs[[2]int{i, j}]; exists {
+				continue
+			}
+			uniquePairs[[2]int{i, j}] = struct{}{}
+			pairs = append(pairs, [2]Point3D{points[i], points[j]})
+		}
+	}
+
+	sort.SliceStable(pairs, func(i, j int) bool {
+		return distance3D(pairs[i][0], pairs[i][1]) < distance3D(pairs[j][0], pairs[j][1])
+	})
+
+	circuits := []Circuit{}
+	for _, pair := range pairs {
+		found := false
+		for i, circuit := range circuits {
+			if slices.Contains(circuit.Points, pair[0].Index) || slices.Contains(circuit.Points, pair[1].Index) {
+				if !slices.Contains(circuit.Points, pair[1].Index) {
+					circuits[i].Points = append(circuits[i].Points, pair[1].Index)
+					fmt.Printf("Adding points %d and %d to circuit %d\n", pair[0].Index, pair[1].Index, i)
+				}
+				if !slices.Contains(circuit.Points, pair[0].Index) {
+					circuits[i].Points = append(circuits[i].Points, pair[0].Index)
+					fmt.Printf("Adding points %d and %d to circuit %d\n", pair[0].Index, pair[1].Index, i)
+				}
+
+				found = true
+
+				break
+			}
+		}
+
+		if !found {
+			circuits = append(circuits, Circuit{Points: []int{pair[0].Index, pair[1].Index}})
+		}
+	}
+
+	for {
+		merged := false
+
+		for i, c1 := range circuits {
+			for j, c2 := range circuits {
+				if i == j {
+					continue
+				}
+
+				for _, p1 := range c1.Points {
+					if slices.Contains(c2.Points, p1) {
+						for _, p := range c2.Points {
+							if !slices.Contains(circuits[i].Points, p) {
+								circuits[i].Points = append(circuits[i].Points, p)
+								fmt.Printf("Merging circuit %d with circuit %d, adding point %d\n", i, j, p)
+							}
+						}
+						circuits = append(circuits[:j], circuits[j+1:]...)
+						merged = true
+						break
+					}
+				}
+				if merged {
+					break
+				}
+			}
+			if merged {
+				break
+			}
+		}
+
+		if !merged {
+			break
+		}
+	}
+
+	sort.SliceStable(circuits, func(i, j int) bool {
+		return len(circuits[i].Points) > len(circuits[j].Points)
+	})
+
+	fmt.Printf("Circuits: %v\n", circuits)
+	fmt.Printf("Total circuits: %d\n", len(circuits))
+}
+
+func distance3D(p1, p2 Point3D) float64 {
+	return math.Sqrt(math.Pow(float64(p1.X-p2.X), 2) +
+		math.Pow(float64(p1.Y-p2.Y), 2) +
+		math.Pow(float64(p1.Z-p2.Z), 2))
+}
